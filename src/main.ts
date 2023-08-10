c988ad5348320856dfadd5f388e07d4793d5666b
nexthis <pawel.romanowski000@gmail.com>
2023-08-10 02:12:57 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,20 @@ import flatpickr from "flatpickr";
 import { Polish } from "flatpickr/dist/l10n/pl"
 
 
-window.addEventListener("load", () => {
+// window.addEventListener("load", () => {
 
-const elements = document.querySelectorAll<HTMLInputElement>('[data-flatpickr]')
+    const elements = document.querySelectorAll<HTMLInputElement>('.flatpickr')
 
 
-for (const item of elements) {
-    let config = { time_24hr: true, locale: Polish, minDate: "today"} ;
-    try{
-        config = {...config, ...JSON.parse(item.dataset.flatpickr ?? "")};
+    for (const item of elements) {
+        let config = { time_24hr: true, locale: Polish, minDate: "today", inline: true} ;
+        try{
+            config = {...config, ...JSON.parse(item.dataset.flatpickr ?? "")};
+        }
+        catch(e){
+            console.log();
+        }
+        flatpickr(item, config);
     }
-    catch(e){
-        console.log();
-    }
-    flatpickr(item, config);
-}
 
-})
+// })
